459728245c2392e03969e608a35357ddaf642724
Jackson452261 <a411758@gmail.com>
2025-08-31 22:52:24 +0800

    移除latesst blog

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -146,7 +146,7 @@ const Blog: React.FC = () => {
           </div>
         )}
 
-        {/* Regular Posts */}
+        {/* Regular Posts  
         <div>
           <h3 className="text-2xl font-bold text-gray-900 mb-8">Latest Articles</h3>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -186,7 +186,7 @@ const Blog: React.FC = () => {
               </article>
             ))}
           </div>
-        </div>
+        </div>*/}
       </div>
     </section>
   );
